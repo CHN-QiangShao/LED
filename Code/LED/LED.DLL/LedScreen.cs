8deaa8908291f2c96f3886e483b49c11b15967de
強少 <wangyuqiang@innorev.com.cn>
2025-04-12 22:50:38 +0800

    2025 年 3 月 15 日修改：
    1、删除无关文件；
    2、由于不知道数据源的字符数，并且实时采集函数无法在显示屏实现滚动显示，所以使用编辑软件/QyLed_V2.47.exe 新建 192.168.0.100-内码文字-English.prg；
    3、在项目中涉及到实时采集函数所需要的要素全部更改。

--- Code/LED/LED.DLL/LedScreen.cs
+++ Code/LED/LED.DLL/LedScreen.cs
@@ -8,17 +8,35 @@ public class LedScreen
     private string _ip;         // 控制卡 ip 地址
     private int _netProtocol;   // 网络通信协议
     private int _uid;           // 种类编号
-    private int _color;         // 字体颜色
+    private int _areaWidth;     // 区域宽度
+    private int _areaHeight;    // 区域高度
+    private int _screenColor;   // 显示屏颜色
+    private int _showStyle;     // 显示方式
+    private int _showSpeed;     // 显示速度
+    private int _stopTime;      // 停留时间
+    private int _fontColor;     // 字体颜色
     private int _font;          // 字体
     private int _size;          // 字体大小
+    private int _updateStyle;   // 更新方式
+    private int _powerOffSave;  // 是否掉电保存
+    private int _rotateMode;    // 旋转模式
 
     // 定义成员属性
     public string Ip { get => _ip; set => _ip = value; }
     public int NetProtocol { get => _netProtocol; set => _netProtocol = value; }
     public int Uid { get => _uid; set => _uid = value; }
-    public int Color { get => _color; set => _color = value; }
+    public int AreaWidth { get => _areaWidth; set => _areaWidth = value; }
+    public int AreaHeight { get => _areaHeight; set => _areaHeight = value; }
+    public int ScreenColor { get => _screenColor; set => _screenColor = value; }
+    public int ShowStyle { get => _showStyle; set => _showStyle = value; }
+    public int ShowSpeed { get => _showSpeed; set => _showSpeed = value; }
+    public int StopTime { get => _stopTime; set => _stopTime = value; }
+    public int FontColor { get => _fontColor; set => _fontColor = value; }
     public int Font { get => _font; set => _font = value; }
     public int Size { get => _size; set => _size = value; }
+    public int UpdateStyle { get => _updateStyle; set => _updateStyle = value; }
+    public int PowerOffSave { get => _powerOffSave; set => _powerOffSave = value; }
+    public int RotateMode { get => _rotateMode; set => _rotateMode = value; }
 
 
     //// 构造方法
@@ -43,7 +61,11 @@ public class LedScreen
         for (int i = 0; i < 4; i++)
         {
             // 调用非托管函数
-            ret = QYLED_DLL.SendCollectionData_Net(content[i], ledValue.controlCardIP, ledValue.UDP, ledValue.uid + i, ledValue.green, ledValue.songFont, ledValue.twelveSquared);
+            //ret = QYLED_DLL.SendCollectionData_Net(content[i], ledValue.controlCardIP, ledValue.UDP, ledValue.uid + i, ledValue.green, ledValue.songFont, ledValue.twelveSquared);
+            ret = QYLED_DLL.SendInternalText_Net(content[i], ledValue.controlCardIP, ledValue.UDP, ledValue.areaWidth, ledValue.areaHeight,
+                                    ledValue.uid + i, ledValue.singleFundamentalColor, ledValue.moveFromRightToLeft, 
+                                    ledValue.showSpeedSlowest, ledValue.cyclicShow, ledValue.red, ledValue.songFont,
+                                    ledValue.twelveSquared, ledValue.updatedThisImmediately, ledValue.saveFalse, ledValue.nonRotate);
             if (ret == 0)
             {
                 Console.WriteLine("成功");
--- Code/LED/LED.DLL/Share/LedValue.cs
+++ Code/LED/LED.DLL/Share/LedValue.cs
@@ -18,11 +18,68 @@ public class LedValue
     public int TCP = 2; // TCP 协议
 
     /// <summary>
+    /// 区域宽度，要求：
+    /// <br>1、参数需要与所建模板的内码文字一致</br>
+    /// <br>2、不大于屏宽</br>
+    /// <br>3、必须是 8 的倍数</br>
+    /// <br>4、最小值是 16</br>
+    /// </summary>
+    public int areaWidth = 72;
+
+    /// <summary>
+    /// 区域高度，要求：
+    /// <br>1、参数需要与所建模板的内码文字一致</br>
+    /// <br>2、不大于屏高</br>
+    /// <br>3、最小值是 16</br>
+    /// </summary>
+    public int areaHeight = 64;
+
+    /// <summary>
     /// 内码文字 ，要求：
     /// <br>1、参数需要与所建模板的内码文字一致</br>
     /// <br>2、值唯一</br>
     /// </summary>
     public int uid = 1;
+
+    // 显示屏颜色 screenColor，要求参数需要与所建模板的内码文字一致
+    public int singleFundamentalColor   = 1; // 单基色
+    public int twoFundamentalColor      = 2; // 双基色
+    public int threeFundamentalColors   = 3; // 三基色
+
+    // 显示方式 showStyle
+    public int showStyleAdaptive                = 0;  // 自适应(系统自动配置)
+    public int moveFromRightToLeft              = 1;  // 从右向左移动
+    public int moveFromLeftToRight              = 2;  // 从左向右移动
+    public int moveFromBottomToTop              = 3;  // 从下向上移动
+    public int moveFromTopToBottom              = 4;  // 从上向下移动
+    public int expandFromRightToLeft            = 5;  // 从右向左展开
+    public int expandFromLeftToRight            = 6;  // 从左向右展开
+    public int expandFromBottomToTop            = 7;  // 从下向上展开
+    public int expandFromTopToBottom            = 8;  // 从上向下展开
+    public int showNow                          = 9;  // 立即显示
+    public int expandFromSidesToCenter          = 10; // 从中间向两边展开
+    public int expandFromCenterToSides          = 11; // 从两边向中间展开
+    public int expandFromCenterToTopAndBottom   = 12; // 从中间向上下展开
+    public int expandFromCenterToBottomAndTop   = 13; // 从上下向中间展开
+    public int blink                            = 14; // 闪烁
+    public int rightLouver                      = 15; // 右百叶
+    public int bottomLouvre                     = 16; // 下百叶
+
+    // 显示速度 showSpeed，数值越大，移动速度越慢
+    public int showSpeedAdaptive    = 0; // 自适应(控制卡自动配置)
+    public int showSpeedFastest     = 1;
+    public int showSpeedTwo         = 2;
+    public int showSpeedThree       = 3;
+    public int showSpeedFour        = 4;
+    public int showSpeedFive        = 5;
+    public int showSpeedSix         = 6;
+    public int showSpeedSeven       = 7;
+    public int showSpeedSlowest     = 8;
+
+    // 停留时间 stopTime
+    public int cyclicShow       = 0;   // 由程序依据播放方式和每屏的显示字符数自动设定(每屏内容不停留, 循环播放)
+    public int unit             = 1;   //  N*5 (以 5 秒钟为一个单位)
+    public int stationaryShow   = 255; // 一直静止显示
 
     // 字体颜色 fontColor
     public int red      = 1; // 红色
@@ -45,4 +102,18 @@ public class LedValue
     public int sixtyFourSquared     = 5; // 64×64 点阵
     public int eightySquared        = 6; // 80×80 点阵
     public int ninetySixSquared     = 7; // 96×96 点阵
+
+    // 更新方式 updateStyle
+    public int updatedAllImmediately    = 1; // 发送完毕所有素材立即更新
+    public int updatedThisImmediately   = 2; // 发送完毕后本素材立即更新，其他素材显示不变
+    public int normalDisplay            = 3; // 发送完毕后本素材不立即更新，按正常显示进行
+
+    // 是否掉电保存 powerOffSave
+    public bool saveTrue  = true;   // 是
+    public bool saveFalse = false;  // 否
+
+    // 旋转模式 rotateMode
+    public int nonRotate                = 0; // 不旋转
+    public int singleCharacterRotate    = 1; // 单字旋转
+    public int areaRotate               = 2; // 区域旋转
 }
